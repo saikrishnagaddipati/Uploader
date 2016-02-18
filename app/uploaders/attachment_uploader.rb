6341f67414d94bdec55f9e112b7e790dab06fb8f
sai krishna <gskrishna44@gmail.com>
2016-02-17 23:54:07 -0600

    loading jpeg and png

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -1,11 +1,11 @@
 class AttachmentUploader < CarrierWave::Uploader::Base
-   storage :file
+   storage :file #change this to fog when you want to upload the files to amazon or heroku
    
    def store_dir
       "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
    end
    
    def extension_white_list
-      %w(pdf doc htm html docx jpg)
+      %w(pdf doc htm html docx jpg png rb mp3)
    end
 end
